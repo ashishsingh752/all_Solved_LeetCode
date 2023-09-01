725e644f6622e6a5a2c840fdbac8259389e23c03
Ashish Singh <99067547+ashishsingh752@users.noreply.github.com>
2023-09-01 23:45:55 +0530

    Time: 6 ms (56.68%) | Memory: 7.9 MB (65.80%) - LeetSync

---
+++ 338-counting-bits/counting-bits.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> v(n+1);
+        for(int i = 0;i<n+1;i++ ){
+            v[i] = __builtin_popcount(i);
+        }
+        return v;
+    }
+};
