49017426575f80fc6fd4568fc1a24976e1f4d573
Ashish Singh <99067547+ashishsingh752@users.noreply.github.com>
2023-10-25 23:08:50 +0530

    Time: 0 ms (100.00%) | Memory: 6.9 MB (41.71%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -1,14 +1,11 @@
 class Solution {
-public:
-    string addBinary(string a, string b) {
+    private:
+    string f(string a, string b){
         int n1=a.size(), n2=b.size();
-    
-         
         reverse(a.begin(), a.end());
         reverse(b.begin(), b.end());
         string str="";
         int c=0;
-        if(n1>n2){
         for(int i=0;i<n1;i++){
             if(i<n2){
                int p=a[i]-'0';
@@ -27,28 +24,14 @@ public:
         if(c){
             str.push_back(c+'0');
         }
-        }
-        else{
-                for(int i=0;i<n2;i++){
-                    if(i<n1){
-                    int p=a[i]-'0';
-                    int q=b[i]-'0'; 
-                    int digit = (p+q+c)%2;
-                    c=(p+q+c)/2;
-                    str.push_back(digit+'0');
-                    }
-                    else{
-                        int val = (b[i]-'0'+c);
-                        int di = val%2;
-                        c=val/2;
-                        str.push_back(di+'0');
-                    }
-                }
-                if(c){
-                    str.push_back(c+'0');
-                }
-        }
         reverse(str.begin(), str.end());
         return str;
+    }
+public:
+    string addBinary(string a, string b) {
+      int n1=a.size();
+      int  n2=b.size();
+      if(n1>n2) return f(a, b);
+      else return f(b, a);
     }
 };
