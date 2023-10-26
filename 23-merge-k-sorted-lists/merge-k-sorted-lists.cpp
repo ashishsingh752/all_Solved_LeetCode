61496b7e14bad1641ac9352129ced8a2880cb389
Ashish Singh <99067547+ashishsingh752@users.noreply.github.com>
2023-10-26 23:36:37 +0530

    Time: 217 ms (10.13%) | Memory: 19.9 MB (9.13%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -9,64 +9,105 @@
  * };
  */
 class Solution {
+     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+       ListNode* dummy = new ListNode(0);
+       ListNode* curr= dummy;
+       ListNode* t1=list1;
+       ListNode* t2=list2;
+    while(t1 || t2){
+        if(t1 && t2){
+            if(t1->val >=t2->val){
+                curr->next= new ListNode(t2->val);
+                curr=curr->next;
+                t2=t2->next;
+            }else{
+                curr->next= new ListNode(t1->val);
+                curr=curr->next;
+                t1=t1->next;
+            }
+    }
+        else if(t1 && t2==NULL){
+                curr->next= new ListNode(t1->val);
+                curr=curr->next;
+                t1=t1->next;
+        }
+        else if(t2 && t1==NULL){
+                curr->next= new ListNode(t2->val);
+                curr=curr->next;
+                t2=t2->next;
+        }
+    }
+     return dummy->next;
+    }
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        vector<vector<int>>v;
-        ListNode* ansList= new ListNode(0);
-        ListNode* temp = ansList;
+       int i=0;
+       if(lists.empty()) return nullptr;
 
-        for(int i=0;i<lists.size();i++){
-             ListNode* head = lists[i];
-             vector<int>temp;
-             while(head){
-                 temp.push_back(head->val);
-                 head=head->next;
-             }
-             head=NULL;
-             v.push_back(temp);
-             temp.clear();
-        }
+       while(lists.size()>1){
+           ListNode* temp =mergeTwoLists(lists[0], lists[1]);
+           lists.push_back(temp);
+           lists.erase(lists.begin());
+           lists.erase(lists.begin());
+       }
 
-        // for(int i =0;i<v.size();i++){
-        //     for(auto it: v[i]) {
-        //         cout<<it<<" ";
-        //     }cout<<endl;
-        // }
+      return lists.front();
+//         vector<vector<int>>v;
+//         ListNode* ansList= new ListNode(0);
+//         ListNode* temp = ansList;
 
-        int n= v.size();  // row
-priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>>pq;
+//         for(int i=0;i<lists.size();i++){
+//              ListNode* head = lists[i];
+//              vector<int>temp;
+//              while(head){
+//                  temp.push_back(head->val);
+//                  head=head->next;
+//              }
+//              head=NULL;
+//              v.push_back(temp);
+//              temp.clear();
+//         }
+
+//         // for(int i =0;i<v.size();i++){
+//         //     for(auto it: v[i]) {
+//         //         cout<<it<<" ";
+//         //     }cout<<endl;
+//         // }
+
+//         int n= v.size();  // row
+// priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>>pq;
        
-        for(int i =0;i<n;i++){
-            if(v[i].size()>=1)pq.push({v[i][0], i});
-            // cout<<"ashish"<<endl;
-        }
+//         for(int i =0;i<n;i++){
+//             if(v[i].size()>=1)pq.push({v[i][0], i});
+//             // cout<<"ashish"<<endl;
+//         }
 
-        vector<int>ans;
-        // while(!pq.empty()){
-        //     cout<<pq.top().first<<" "<<pq.top().second<<endl;
-        //     pq.pop();
-        // }
+//         vector<int>ans;
+//         // while(!pq.empty()){
+//         //     cout<<pq.top().first<<" "<<pq.top().second<<endl;
+//         //     pq.pop();
+//         // }
         
-        while(!pq.empty()){
-            int element = pq.top().first;
-            int index = pq.top().second;
-            pq.pop();
+//         while(!pq.empty()){
+//             int element = pq.top().first;
+//             int index = pq.top().second;
+//             pq.pop();
             
-            ans.push_back(element);
-            v[index].erase(v[index].begin(), v[index].begin()+1);
+//             ans.push_back(element);
+//             v[index].erase(v[index].begin(), v[index].begin()+1);
 
 
-            if(v[index].size()>=1){
-              if(v.size()>=1)  pq.push({v[index][0], index});
-            }
-        }
+//             if(v[index].size()>=1){
+//               if(v.size()>=1)  pq.push({v[index][0], index});
+//             }
+//         }
 
-        for(auto it: ans){
-            ListNode*  dummy = new ListNode(it);
-            temp->next = dummy;
-            temp= temp->next;
-        }
-        return ansList->next;
+//         for(auto it: ans){
+//             ListNode*  dummy = new ListNode(it);
+//             temp->next = dummy;
+//             temp= temp->next;
+//         }
+//         return ansList->next;
         // return ansList->next;
     }
 };
