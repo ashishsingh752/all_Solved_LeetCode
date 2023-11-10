cf6910019da502ff6f2e34bcaa0c3bd433b027a4
Ashish Singh <99067547+ashishsingh752@users.noreply.github.com>
2023-11-10 23:45:53 +0530

    Time: 422 ms (56.97%) | Memory: 157.7 MB (30.30%) - LeetSync

--- 1866-restore-the-array-from-adjacent-pairs/restore-the-array-from-adjacent-pairs.cpp
+++ 1866-restore-the-array-from-adjacent-pairs/restore-the-array-from-adjacent-pairs.cpp
@@ -1,46 +1,41 @@
 class Solution {
 public:
-    vector<int> restoreArray(vector<vector<int>>& adjacentPairs){
-       unordered_map<int, vector<int>>mp; 
-       vector<int>ans;
-       vector<int>temp ; 
-       for(auto it: adjacentPairs){
-           mp[it[0]].push_back(it[1]);
-           mp[it[1]].push_back(it[0]);
-       }
+    vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
+        unordered_map<int, vector<int>> mp; 
+        vector<int> ans;
+        unordered_set<int> seen;
 
-       for(auto it: mp){
-           if(it.second.size()==1){
-               temp.push_back(it.first);
-           }
-       }
-       for(auto it:temp)cout<<it<<" ";
-
-       map<int , bool>m; 
-        int val = temp[0];
-        // ans.push_back(val);
-        m[val]=true;
-        int n= adjacentPairs.size()+1;
-        
-        while(ans.size()!=n){
-            // if(!m[val]){
-            //     m[val]=true;
-            //     if(m[mp[val]])
-            // }
-            auto it = mp[val];
-            ans.push_back(val);
-            if(!m[it[0]]){
-                int t = it[0];
-                val=t;
-                m[val]=true;
-            }
-            else if(it.size()==2 &&  !m[it[1]]){
-                val  = it[1];
-                m[val]=true;
-            }
-
+        // Build the adjacency list
+        for(auto& pair : adjacentPairs) {
+            mp[pair[0]].push_back(pair[1]);
+            mp[pair[1]].push_back(pair[0]);
         }
-       return ans;
 
+        // Find the starting point (a node with only one neighbor)
+        int start = 0;
+        for(auto& entry : mp) {
+            if(entry.second.size() == 1) {
+                start = entry.first;
+                break;
+            }
+        }
+
+        // DFS to reconstruct the array
+        dfs(mp, start, seen, ans);
+
+        return ans;
+    }
+
+private:
+    void dfs(unordered_map<int, vector<int>>& mp, int node, unordered_set<int>& seen, vector<int>& ans) {
+        ans.push_back(node);
+        seen.insert(node);
+
+        for(int neighbor : mp[node]) {
+            if(seen.find(neighbor) == seen.end()) {
+                dfs(mp, neighbor, seen, ans);
+                break;  // Since each node has at most two neighbors, no need to explore further
+            }
+        }
     }
 };
