aedf02e5822f6392296cfc696232cac809b41f2f
Ashish Singh <99067547+ashishsingh752@users.noreply.github.com>
2024-01-02 15:02:12 +0530

    Time: 24 ms (8.02%) | Memory: 29.8 MB (92.71%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
-        map<int,int>mp;
-        int cnt=0;
+        map<int, int> mp;
+        int cnt = 0;
         int size = nums.size();
-        for(int i=0;i<size; i++){
-            mp[nums[i]]++;
-            cnt = max(cnt, mp[nums[i]]);
+        
+        for (auto it : nums) {
+            mp[it]++;
+            cnt = max(cnt, mp[it]);
         }
-        vector<vector<int>>ans(cnt);
-        for(auto it: mp){
-          int j=0;
-            while(it.second){
-                ans[j].push_back(it.first);
-                it.second--;
-                j++;
-            }
+      
+        vector<vector<int>> ans(cnt);
+        vector<int> v(size+1, 0);
+       
+        for (int i = 0; i < size; i++) {
+            ans[v[nums[i]]].push_back(nums[i]);
+            v[nums[i]]++;
         }
+
+        // for(auto it: mp){
+        //   int j= 0;
+        //     while(it.second){
+        //         ans[j].push_back(it.first);
+        //         it.second--;
+        //         j++;
+        //     }
+        // }
+
         return ans;
     }
-
 };
